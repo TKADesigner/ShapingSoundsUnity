62d09839a600de514d9b9c9edf45cb5cbee1cd68
jeremysuh02 <jeremysuh@utexas.edu>
2022-11-14 11:44:41 -0600

    spawn for every available shape

--- Assets/Scripts/ShapeSpawn.cs
+++ Assets/Scripts/ShapeSpawn.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class ShapeSpawn : MonoBehaviour
 {
     public GameObject shape;
+    public GameObject shape1;
+    public GameObject shape2;
     public GameObject spawn;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             GameObject newShape = Instantiate(shape);
+            newShape.transform.position = spawn.transform.position + new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject newShape = Instantiate(shape1);
+            newShape.transform.position = spawn.transform.position + new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            GameObject newShape = Instantiate(shape2);
             newShape.transform.position = spawn.transform.position + new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
         }
     }
